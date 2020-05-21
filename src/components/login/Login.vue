06c0d06097429d0611f0dcd84f8b87df2873ca9e
pickway <287917815@qq.com>
2020-05-22 00:37:16 +0800

    样式调整

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -12,8 +12,10 @@
       </label>
       <!--<button class="login_btn el-button el-button&#45;&#45;primary is-round" type="primary" round>登录</button>-->
       <el-button class="login_btn" @click.native="login" type="primary" round :loading="isBtnLoading">登录</el-button>
+      <el-button class="login_btn" @click.native="regist" type="primary" round :loading="isBtnLoading">注册</el-button>
       <div style="margin-top: 10px">
-        <span style="color: #000099;" @click="login">司机账号登陆</span><span style="float: right;color: #A9A9AB">忘记密码？</span>
+        <span style="color: #000099;margin-left: 50%;" @click="login">游客登录</span>
+        <span style="color: #A9A9AB;margin-left: 13%;">忘记密码？</span>
       </div>
     </div>
   </div>
@@ -68,20 +70,20 @@ export default {
 
 <style>
   .login_form {
-    padding-top: 10%;
+    padding-top: 5%;
     padding-left: 10%;
     padding-right: 10%;
   }
 
   .qxs-ic_user {
     background: url("../../assets/login/user.png") no-repeat;
-    background-size: 13px 15px;
+    background-size: 15px 16px;
     background-position: 3%;
   }
 
   .qxs-ic_password {
     background: url("../../assets/login/password.png") no-repeat;
-    background-size: 13px 15px;
+    background-size: 15px 16px;
     background-position: 3%;
     margin-bottom: 20px;
   }
@@ -91,12 +93,20 @@ export default {
   }
 
   .login_btn {
-    width: 100%;
+    width: 7%;
     font-size: 16px;
+    margin-left: 54%;
     background: -webkit-linear-gradient(left, #000099, #2154FA); /* Safari 5.1 - 6.0 */
     background: -o-linear-gradient(right, #000099, #2154FA); /* Opera 11.1 - 12.0 */
     background: -moz-linear-gradient(right, #000099, #2154FA); /* Firefox 3.6 - 15 */
     background: linear-gradient(to right, #000099, #2154FA); /* 标准的语法 */
     filter: brightness(1.4);
+  }
+  .qxs-icon {
+    width: 20%;
+    padding-left: 4%;
+    margin-left: 50%;
+    margin-right: 30%;
+    border: 0.1px solid black;
   }
 </style>
